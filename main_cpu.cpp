bf8a6f7bcfbcbdd3f73ea311113e312315e08f7f
rgreen32 <ray.greenm@gmail.com>
2022-02-07 02:04:18 -0600

    IOUFilter working (mostly)

--- main_cpu.cpp
+++ main_cpu.cpp
@@ -2,6 +2,8 @@
 #include <opencv2/dnn.hpp>
 #include <opencv2/imgproc.hpp>
 #include <iostream>
+#include <algorithm>
+#include <tuple>
 
 constexpr float CONFIDENCE_THRESHOLD = 0.9;
 constexpr float NMS_THRESHOLD = 0.4;
@@ -13,6 +15,8 @@ const cv::Scalar colors[] = {
     {255, 0, 0}};
 const auto NUM_COLORS = sizeof(colors) / sizeof(colors[0]);
 
+bool myfunction(std::tuple<cv::Rect, float> i, std::tuple<cv::Rect, float> j) { return (std::get<1>(i) > std::get<1>(j)); }
+
 int FindHighestScoringDetectionBox(std::vector<cv::Rect> class_detection_boxes, std::vector<float> class_detection_scores)
 {
     float highest_confidence_score = -INFINITY;
@@ -20,11 +24,11 @@ int FindHighestScoringDetectionBox(std::vector<cv::Rect> class_detection_boxes, 
 
     //find highest scoring box
     int tes1 = class_detection_boxes.size();
-    for(int class_detection_boxes_index = 0; class_detection_boxes_index < class_detection_boxes.size(); class_detection_boxes_index++)
+    for (int class_detection_boxes_index = 0; class_detection_boxes_index < class_detection_boxes.size(); class_detection_boxes_index++)
     {
         cv::Rect detection_box = class_detection_boxes[class_detection_boxes_index];
         float detection_box_confidence_score = class_detection_scores[class_detection_boxes_index];
-        if(detection_box_confidence_score > highest_confidence_score)
+        if (detection_box_confidence_score > highest_confidence_score)
         {
             highest_confidence_score = detection_box_confidence_score;
             most_confident_box_index = class_detection_boxes_index;
@@ -33,86 +37,75 @@ int FindHighestScoringDetectionBox(std::vector<cv::Rect> class_detection_boxes, 
     return most_confident_box_index;
 }
 
-void IOUFilter(cv::Rect most_confident_box, std::vector<cv::Rect> class_detection_boxes, std::vector<float> class_detection_scores, float IOUThreshHold, std::vector<cv::Rect> filtered_class_detection_boxes)
+void NonMaxSupression(std::vector<std::tuple<cv::Rect, float>> (&all_detection_boxes)[NUM_CLASSES], float IOUThreshold)
 {
-    int class_detection_boxes_index = 0;
-    while(class_detection_boxes_index < class_detection_boxes.size())
+
+    std::vector<cv::Rect> filtered_detection_boxes[NUM_CLASSES];
+    std::vector<float> filtered_detection_scores[NUM_CLASSES];
+
+    for (int all_detection_boxes_index = 0; all_detection_boxes_index < NUM_CLASSES; all_detection_boxes_index++)
     {
-        cv::Rect detection_box = class_detection_boxes[class_detection_boxes_index];
-        if((detection_box.x < most_confident_box.x) && (detection_box.x + detection_box.width < most_confident_box.x + most_confident_box.width) 
-            || (detection_box.x > most_confident_box.x + most_confident_box.width))
+
+        std::vector<std::tuple<cv::Rect, float>> &class_detection_boxes = all_detection_boxes[all_detection_boxes_index];
+        std::sort(class_detection_boxes.begin(), class_detection_boxes.end(), myfunction);
+        if (class_detection_boxes.size() == 0)
         {
-            class_detection_boxes_index++;
-            continue;  
-        }
-        else if((detection_box.y < most_confident_box.y) && (detection_box.y + detection_box.height < most_confident_box.y + most_confident_box.height)
-            || (detection_box.y > most_confident_box.y + most_confident_box.height))
-        {
-            class_detection_boxes_index++;
             continue;
         }
-        int x_overlap_min_boundary = std::min(detection_box.x, most_confident_box.x);
-        int x_overlap_max_boundary = std::max(detection_box.x + detection_box.width, most_confident_box.x + most_confident_box.width);
-        int max_x_overlap = x_overlap_max_boundary - x_overlap_min_boundary;
-        max_x_overlap = max_x_overlap - (detection_box.x - x_overlap_min_boundary);
-        max_x_overlap = max_x_overlap  - (x_overlap_max_boundary - detection_box.x + detection_box.width);
 
-        int y_overlap_min_boundary = std::min(detection_box.y, most_confident_box.y);
-        int y_overlap_max_boundary = std::max(detection_box.y + detection_box.height, most_confident_box.y + most_confident_box.height);
-        int max_y_overlap = y_overlap_max_boundary - y_overlap_min_boundary;
-        max_y_overlap = max_y_overlap - (detection_box.y - y_overlap_min_boundary);
-        max_y_overlap = max_y_overlap  - (y_overlap_max_boundary - detection_box.y + detection_box.height);
 
-        int overlap_area = max_x_overlap * max_y_overlap;
-        float overlap_percentage = overlap_area/detection_box.area();
-        if(overlap_percentage > IOUThreshHold)
+
+        int class_detection_boxes_index = 0;
+        while (class_detection_boxes_index < class_detection_boxes.size())
         {
-            // filtered_class_detection_boxes.push_back(detection_box);
-            class_detection_boxes.erase(class_detection_boxes.begin() + class_detection_boxes_index);
-            class_detection_scores.erase(class_detection_scores.begin() + class_detection_boxes_index);
-        }else
-        {
+            cv::Rect most_confident_box = std::get<0>(class_detection_boxes[class_detection_boxes_index]);
+            float most_confident_box_score = std::get<1>(class_detection_boxes[class_detection_boxes_index]);
+            int filter_index = class_detection_boxes_index + 1;
+            while (filter_index < class_detection_boxes.size())
+            {
+
+                cv::Rect detection_box = std::get<0>(class_detection_boxes[filter_index]);
+                float detection_box_condfidence = std::get<1>(class_detection_boxes[filter_index]);
+                if ((detection_box.x < most_confident_box.x) && (detection_box.x + detection_box.width < most_confident_box.x + most_confident_box.width) || (detection_box.x > most_confident_box.x + most_confident_box.width))
+                {
+                    filter_index++;
+                    // most_confident_box = std::get<0>(class_detection_boxes[class_detection_boxes_index]);
+                    // most_confident_box_score = std::get<1>(class_detection_boxes[class_detection_boxes_index]);
+                    continue;
+                }
+                else if ((detection_box.y < most_confident_box.y) && (detection_box.y + detection_box.height < most_confident_box.y + most_confident_box.height) || (detection_box.y > most_confident_box.y + most_confident_box.height))
+                {
+                    filter_index++;
+                    continue;
+                }
+                int x_overlap_min_boundary = std::min(detection_box.x, most_confident_box.x);
+                int x_overlap_max_boundary = std::max(detection_box.x + detection_box.width, most_confident_box.x + most_confident_box.width);
+                int max_x_overlap = x_overlap_max_boundary - x_overlap_min_boundary;
+                max_x_overlap = max_x_overlap - (detection_box.x - x_overlap_min_boundary);
+                max_x_overlap = max_x_overlap - (x_overlap_max_boundary - detection_box.x + detection_box.width);
+
+                int y_overlap_min_boundary = std::min(detection_box.y, most_confident_box.y);
+                int y_overlap_max_boundary = std::max(detection_box.y + detection_box.height, most_confident_box.y + most_confident_box.height);
+                int max_y_overlap = y_overlap_max_boundary - y_overlap_min_boundary;
+                max_y_overlap = max_y_overlap - (detection_box.y - y_overlap_min_boundary);
+                max_y_overlap = max_y_overlap - (y_overlap_max_boundary - detection_box.y + detection_box.height);
+
+                int overlap_area = max_x_overlap * max_y_overlap;
+                float overlap_percentage = overlap_area / detection_box.area();
+                if (overlap_percentage > IOUThreshold)
+                {
+                    // filtered_class_detection_boxes.push_back(detection_box);
+                    class_detection_boxes.erase(class_detection_boxes.begin() + filter_index);
+                }
+                else
+                {
+                    filter_index++;
+                }
+            }
             class_detection_boxes_index++;
         }
     }
 }
-
-void NonMaxSupression(std::vector<cv::Rect> all_detection_boxes[NUM_CLASSES], std::vector<float> all_detection_scores[NUM_CLASSES], std::vector<cv::Rect> filtered_detection_boxes[NUM_CLASSES], std::vector<float> filtered_detection_scores[NUM_CLASSES])
-{
-    for(int all_detection_boxes_index = 0; all_detection_boxes_index < NUM_CLASSES; all_detection_boxes_index++)
-    {
-
-        std::vector<cv::Rect> class_detection_boxes = all_detection_boxes[all_detection_boxes_index];
-        std::vector<float> class_detection_scores = all_detection_scores[all_detection_boxes_index];
-
-            
-        while(class_detection_boxes.size() > 0)
-        {
-            int most_confident_box_index = FindHighestScoringDetectionBox(class_detection_boxes, class_detection_scores);
-            cv::Rect most_confident_box = class_detection_boxes[most_confident_box_index];
-
-            filtered_detection_boxes[all_detection_boxes_index].push_back(most_confident_box);
-            filtered_detection_scores[all_detection_boxes_index].push_back(class_detection_scores[most_confident_box_index]);
-            class_detection_boxes.erase(class_detection_boxes.begin() + most_confident_box_index);
-            class_detection_scores.erase(class_detection_scores.begin() + most_confident_box_index);
-
-            IOUFilter(most_confident_box, class_detection_boxes, class_detection_scores, .40, filtered_detection_boxes[all_detection_boxes_index]);
-            // //find highest scoring box
-            // for(int class_detection_boxes_index = 0; class_detection_boxes_index < class_detection_boxes.size(); class_detection_boxes_index++)
-            // {
-            //     cv::Rect detection_box = class_detection_boxes[class_detection_boxes_index];
-            //     float detection_box_confidence_score = class_detection_scores[class_detection_boxes_index];
-            //     if(detection_box_confidence_score > highest_confidence_score)
-            //     {
-            //         highest_confidence_score = detection_box_confidence_score;
-            //         most_confident_box = detection_box;
-            //     }
-            // }
-        }
-        
-    }
-}
-
 
 int main(int argc, char **argv)
 {
@@ -129,13 +122,15 @@ int main(int argc, char **argv)
     neural_net.setPreferableTarget(cv::dnn::DNN_TARGET_CPU);
     auto output_names = neural_net.getUnconnectedOutLayersNames();
     cv::Mat frame;
-    
+
     cv::Mat blob;
-    std::vector<cv::Mat> detections;
+    std::vector<cv::Mat> neural_net_output;
 
     std::vector<int> class_indices[NUM_CLASSES];
-    std::vector<cv::Rect> detection_boxes[NUM_CLASSES];
-    std::vector<float> detection_scores[NUM_CLASSES];
+    // std::vector<cv::Rect> detection_boxes[NUM_CLASSES];
+    // std::vector<float> detection_scores[NUM_CLASSES];
+    std::vector<std::tuple<cv::Rect, float>> all_detections[NUM_CLASSES];
+
     for (;;)
     {
         if (!cap.read(frame))
@@ -154,10 +149,10 @@ int main(int argc, char **argv)
         neural_net.setInput(blob);
 
         // auto dnn_start = std::chrono::steady_clock::now();
-        neural_net.forward(detections, output_names);
+        neural_net.forward(neural_net_output, output_names);
         // auto dnn_end = std::chrono::steady_clock::now();
 
-        for (auto &detection : detections)
+        for (auto &detection : neural_net_output)
         {
             const auto num_boxes = detection.rows;
             for (int i = 0; i < num_boxes; i++)
@@ -173,27 +168,26 @@ int main(int argc, char **argv)
                     auto confidence = *detection.ptr<float>(i, 5 + c);
                     if (confidence >= CONFIDENCE_THRESHOLD)
                     {
-                        detection_boxes[c].push_back(rect);
-                        detection_scores[c].push_back(confidence);
+                        std::tuple<cv::Rect, float> class_detection = std::make_tuple(rect, confidence);
+                        all_detections[c].push_back(class_detection);
                     }
                 }
             }
         }
-        std::vector<cv::Rect> filtered_detection_boxes[NUM_CLASSES];
-        std::vector<float> filtered_detection_scores[NUM_CLASSES];
-        // NonMaxSupression(detection_boxes, detection_scores, filtered_detection_boxes, filtered_detection_scores);
+
+        NonMaxSupression(all_detections, NMS_THRESHOLD);
 
         for (int c = 0; c < NUM_CLASSES; c++)
         {
-            std::vector<cv::Rect> &class_detection_boxes = detection_boxes[c];
-            std::vector<float> class_detection_scores = detection_scores[c];
+            auto &class_detections = all_detections[c];
+            // std::vector<float> class_detection_scores = detection_scores[c];
 
-            for (cv::Rect &class_detection_box: class_detection_boxes)
+            for (auto &class_detection : class_detections)
             {
                 const auto color = colors[c % NUM_COLORS];
-                cv::rectangle(frame, cv::Point(class_detection_box.x, class_detection_box.y), cv::Point(class_detection_box.x + class_detection_box.width, class_detection_box.y + class_detection_box.height), color, 3);
+                cv::rectangle(frame, cv::Point(std::get<0>(class_detection).x, std::get<0>(class_detection).y), cv::Point(std::get<0>(class_detection).x + std::get<0>(class_detection).width, std::get<0>(class_detection).y + std::get<0>(class_detection).height), color, 3);
             }
-            class_detection_boxes.clear();
+            class_detections.clear();
         }
         // auto total_end = std::chrono::steady_clock::now();
 
@@ -203,7 +197,5 @@ int main(int argc, char **argv)
             break;
         }
     }
-
     return 0;
 }
-
